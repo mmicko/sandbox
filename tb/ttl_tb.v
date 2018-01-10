9011856820ab8dd75842fa2ea35a73ea19c12abf
Miodrag Milanovic <mmicko@gmail.com>
2018-01-10 16:35:48 +0100

    test all

--- tb/ttl_tb.v
+++ tb/ttl_tb.v
@@ -4,30 +4,48 @@
 */
 module ttl_tb();
 
-	//wire [7:0] r;
-	//wire [7:0] w;
-	//reg g1;
-	//reg g2;
+	wire [7:0] r1;
+	wire [7:0] w;
+	reg g1;
+	reg g2;
 
-	//reg [2:0] a;
-	//reg e1;
-	//reg e2;
-	//reg e3;
+	reg [2:0] aa;
+	reg e1;
+	reg e2;
+	reg e3;
 
-	//ttl_74244 b(._1G_n(g1),._1A(r[3:0]),._1Y(w[3:0]),._2G_n(g2),._2A(r[7:4]),._2Y(w[7:4]));
+	ttl_74244 buffer(._1G_n(g1),._1A(r1[3:0]),._1Y(w[3:0]),._2G_n(g2),._2A(r1[7:4]),._2Y(w[7:4]));
 	
-	//ttl_74138 dec(.A(a),.E1_n(e1),.E2_n(e2),.E3(e3),.Y(r));
+	ttl_74138 dec(.A(aa),.E1_n(e1),.E2_n(e2),.E3(e3),.Y(r1));
     
+	wire [7:0] r;
 	reg g;
 	reg c;
 	reg a;
 	reg b;
-	wire [7:0] r;
+
 	ttl_74155 dem(._1C(c),._1G_n(g),._2G_n(g),.A(a),.B(b),._2C_n(~c),._1Y(r[3:0]),._2Y(r[7:4]));
 	initial
 	begin
 		$dumpfile("ttl_tb.vcd");
 		$dumpvars(0, ttl_tb);
+
+		e1 = 1;
+		e2 = 1;
+		e3 = 1;
+		g1 = 1;
+		g2 = 1;
+		aa = 3'b000;
+		#10
+		e1 = 0;
+		#20
+		e2 = 0;
+		#50
+		g1 = 0;
+		g2 = 0;
+		#100		
+
+	
 		g = 0;
 		
 		
@@ -64,19 +82,7 @@ module ttl_tb();
 		a = 1;
 		#20
 		g = 1;
-	/*	e1 = 1;
-		e2 = 1;
-		e3 = 1;
-		g1 = 1;
-		g2 = 1;
-		a = 3'b000;
-		#10
-		e1 = 0;
-		#20
-		e2 = 0;
-		#50
-		g1 = 0;
-		g2 = 0;*/
+
 		#100		
 		$finish;
 	end
